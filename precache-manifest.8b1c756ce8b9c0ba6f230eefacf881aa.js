af36ce97222f3e9d89be1a4583656a3ecd384644
Tushna Eduljee <tushna@Tushnas-MacBook-Pro.local>
2020-04-29 16:30:45 -0400

    Updates

---
+++ precache-manifest.8b1c756ce8b9c0ba6f230eefacf881aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11e6a5e9dccab95304e228a808a19dc",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "7057967a47b0027084e4",
+    "url": "/personal_website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2187496d61045b7b61c1",
+    "url": "/personal_website/static/js/2.c9b6e24c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal_website/static/js/2.c9b6e24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7057967a47b0027084e4",
+    "url": "/personal_website/static/js/main.92fb1723.chunk.js"
+  },
+  {
+    "revision": "28f5b87109ed42d9bbd4",
+    "url": "/personal_website/static/js/runtime-main.f3302295.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal_website/static/media/logo.5d5d9eef.svg"
+  }
+]);
